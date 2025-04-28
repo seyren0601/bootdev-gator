f98c92b4d7adb16e8826a7fa9d6c01f2d1bb1f40
ThinhNguyen <sorrenw@gmail.com>
2025-04-28 17:43:40 +0700

    feat: Implemented user registration and login (save infos into postgres database)
    https://www.boot.dev/lessons/8279802c-a867-4ba6-9d06-25625bc42544

--- handlers.go
+++ handlers.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"errors"
+	"fmt"
 	"gator/internal/config"
+	"gator/internal/database"
+	"time"
 )
 
 type state struct {
 	config *config.Config
+	db     *database.Queries
 }
 
 func handlerLogin(s *state, cmd command) error {
@@ -15,11 +20,47 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	username := cmd.parameters[0]
-	s.config.Current_user_name = username
-	err := s.config.SetUser()
+
+	user, err := s.db.GetUser(context.Background(), username)
+	if err != nil {
+		return errors.New("username doesn't exist")
+	}
+
+	s.config.Current_user_name = user.Name
+	err = s.config.SetUser()
 	if err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.parameters) != 1 {
+		return errors.New("register command expects 1 parameters")
+	}
+
+	username := cmd.parameters[0]
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      username,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf(`User created successfully
+	UUID: %s
+	CreatedAt: %s
+	UpdatedAt: %s
+	Name: %s\n
+`, user.ID, user.CreatedAt.Local().String(), user.UpdatedAt.Local().String(), user.Name)
+
+	s.config.Current_user_name = user.Name
+	s.config.SetUser()
 
 	return nil
 }
